c78c0b0907750ea6609bd82f5a0a6867ddd72df8
Terence Montecillo <4hoursleft@gmail.com>
2025-10-10 20:57:00 +0800

    Deleted old files for new

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-    const loader = document.getElementById("loader");
-    loader.style.display = "none";
-});
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    // No Progress here :)
-})
